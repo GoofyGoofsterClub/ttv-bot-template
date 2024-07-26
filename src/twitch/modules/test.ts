30b527e27670d37ad0d4167be22fc5c5f4bddfc0
Mikhail <me@reze.moe>
2024-07-27 07:38:31 +0900

    change: test command result

--- src/twitch/modules/test.ts
+++ src/twitch/modules/test.ts
@@ -5,7 +5,7 @@ export default class TestCommand extends Module {
     public readonly command: string = "test";
 
     async run(channel: string, tags: any, message: string, self: boolean) {
-        console.log("wowzersz....");
+        console.log("Test passed.");
     }
 
 }
